e4c58193fd8f0081c6ae4cd99c9542777b95a89f
Natalia Gutierrez <bgutisol2019@gmail.com>
2022-11-28 19:31:45 -0400

    prueba1

--- e_motorama/settings.py
+++ e_motorama/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'e_motorama.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql', #No se olviden utilizar la información que utilicen en su compu
-        'NAME': "emotorama",
-        'USER': "sa",
-        'PASSWORD': "123456",
-        'HOST': 'DESKTOP-UD0VPHI\MSSQLSERVER1',
+        'ENGINE': 'mssql',
+        'NAME': 'motorama',
+        'USER': 'NataliaProyectoSis',
+        'PASSWORD': '1234',
+        'HOST': 'DESKTOP-JPMRRH8\\NATALIASQLSERVER',
         'PORT': '',
         'OPTIONS': {
-            'driver': 'ODBC Driver 17 for SQL Server',
-        },
+            'driver': 'ODBC Driver 17 for SQL Server'
+        }
     }
 }
 
