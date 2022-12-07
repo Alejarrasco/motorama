19855138e4d5fc9a474e8c1a2a3820c1030180ae
Natalia Gutierrez <bgutisol2019@gmail.com>
2022-12-07 00:00:22 -0400

    mostrarimagenes (sin diseño)

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 #estas url es para estar mas organizado
 from django.urls import path
 from . import views #cuando pones . significa la ruta actual, osea importa de la ruta actual
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns=[
     # path('login/', views.login, name="login"), 
 
@@ -54,3 +55,4 @@ urlpatterns=[
     path('verCarrito/<int:cli>/<int:ven>', views.verCarrito, name='spyCarrito')
     
 ]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
